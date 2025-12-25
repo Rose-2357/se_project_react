a327f12b0c2795a6999c48abc6108832bbb710fe
RoseGhaffari <aradsystem995@gmail.com>
2025-12-25 16:33:11 -0600

    feat: initiate vite, add header section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import "./Header.css";
+import HeaderLogo from "../../assets/logo.svg";
+import HeaderProfileImg from "../../assets/profile-img.svg";
+
+export default function Header() {
+  const currentDate = new Date().toLocaleDateString("default", {
+    month: "long",
+    day: "numeric",
+  });
+  return (
+    <header className="header">
+      <div className="header__column">
+        <figure className="header__img-wrapper header__img-wrapper_type_logo">
+          <img src={HeaderLogo} alt="WTWR logo" className="header__img" />
+        </figure>
+        <p className="header__text">{currentDate}</p>
+      </div>
+      <div className="header__column">
+        <button className="header__btn">+ Add clothes</button>
+        <p className="header__text header__text_type_username">
+          Terrence Tegegne
+        </p>
+        <figure className="header__img-wrapper header__img-wrapper_type_profile">
+          <img src={HeaderProfileImg} alt="" className="header__img" />
+        </figure>
+      </div>
+    </header>
+  );
+}
