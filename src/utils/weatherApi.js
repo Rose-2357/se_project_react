6e307a750c3e3baa5fa3046520a6190b0f1d4cab
RoseGhaffari <aradsystem995@gmail.com>
2025-12-29 18:01:29 -0600

    fix: replace the mock data with real data

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -14,26 +14,18 @@ document.addEventListener("keydown", (e) => {
 });
 
 export function getWeatherData() {
-  // return fetch(
-  //   `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKeey}`
-  // )
-  //   .then((res) =>
-  //     res.ok ? res.json() : Promise.reject(new Error(res.status))
-  //   )
-  //   .then((res) => ({
-  //     city: res.name,
-  //     temp: Math.round(res.main.feels_like),
-  //     weather: res.weather[0].main,
-  //     sunrise: res.sys.sunrise,
-  //     sunset: res.sys.sunset,
-  //   }))
-  //   .catch((res) => Promise.reject(res));
-
-  return Promise.resolve({
-    city: "Garland",
-    temp: temps[num],
-    weather: "Clouds",
-    sunrise: 1766755652,
-    sunset: 1766791601,
-  });
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKeey}`
+  )
+    .then((res) =>
+      res.ok ? res.json() : Promise.reject(new Error(res.status))
+    )
+    .then((res) => ({
+      city: res.name,
+      temp: Math.round(res.main.feels_like),
+      weather: res.weather[0].main,
+      sunrise: res.sys.sunrise,
+      sunset: res.sys.sunset,
+    }))
+    .catch((res) => Promise.reject(res));
 }
